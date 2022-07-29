0273b2464a04b2fe3afc86edd646846aba6e74d5
corsair0909 <870233316@qq.com>
2022-07-29 16:06:41 +0800

    消融效果案例

---
+++ Assets/dissolve/script/Demo_control.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace EasyGameStudio.Disslove_urp
+{
+    public class Demo_control : MonoBehaviour
+    {
+        public GameObject[] game_objects;
+        public AudioSource audio_source;
+        public AudioClip ka;
+        public Text text_title;
+        public string[] string_titles;
+
+        private int index = 0;
+
+        void Start()
+        {
+           
+        }
+
+        public void on_next_btn()
+        {
+            this.index++;
+            if (this.index >= this.game_objects.Length)
+                this.index = 0;
+
+
+            for (int i = 0; i < this.game_objects.Length; i++)
+            {
+                if (i == this.index)
+                {
+                    this.game_objects[i].SetActive(true);
+                }
+                else
+                {
+                    this.game_objects[i].SetActive(false);
+                }
+            }
+
+            this.text_title.text = this.string_titles[this.index];
+
+            this.audio_source.PlayOneShot(this.ka);
+        }
+
+        public void on_previous_btn()
+        {
+            this.index--;
+            if (this.index < 0)
+                this.index = this.game_objects.Length-1;
+
+
+            for (int i = 0; i < this.game_objects.Length; i++)
+            {
+                if (i == this.index)
+                {
+                    this.game_objects[i].SetActive(true);
+                }
+                else
+                {
+                    this.game_objects[i].SetActive(false);
+                }
+            }
+
+            this.text_title.text = this.string_titles[this.index];
+
+            this.audio_source.PlayOneShot(this.ka);
+        }
+    }
+}
---
+++ Assets/dissolve/script/Dissolve.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EasyGameStudio.Disslove_urp
+{
+    public class Dissolve : MonoBehaviour
+    {
+        [Header("show speed")]
+        [Range(0, 5f)]
+        public float speed_show;
+
+        [Header("hide speed")]
+        [Range(0, 5f)]
+        public float hide_show;
+
+        [Header("materials")]
+        public Material[] materials;
+
+        private bool is_showing=false;
+        private bool is_hiding=false;
+        private float threshold = 0;
+
+        [Header("min max threshold")]
+        public float min_threshold;
+        public float max_threshold;
+
+        void OnEnable()
+        {
+            //this.threshold = this.max_threshold;
+            //for (int i = 0; i < this.materials.Length; i++)
+            //{
+            //    this.materials[i].SetFloat("_threshold", this.threshold);
+            //}
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (this.is_showing)
+            {
+                //this.threshold = Mathf.Lerp(this.threshold, this.min_threshold, Time.deltaTime * this.speed_show);
+
+                this.threshold -= Time.deltaTime * this.speed_show;
+
+                if (this.threshold <= this.min_threshold)
+                {
+                    this.threshold = this.min_threshold;
+
+                    this.is_showing = false;
+                }
+
+                for (int i = 0; i < this.materials.Length; i++)
+                {
+                    this.materials[i].SetFloat("_threshold", this.threshold);
+                }
+            }
+
+            if (this.is_hiding)
+            {
+                //this.threshold = Mathf.Lerp(this.threshold, this.max_threshold, Time.deltaTime * this.speed_show);
+
+                this.threshold += Time.deltaTime * this.speed_show;
+
+                if (this.threshold >= this.max_threshold)
+                {
+                    this.threshold = this.max_threshold;
+
+                    this.is_hiding = false;
+                }
+                for (int i = 0; i < this.materials.Length; i++)
+                {
+                    this.materials[i].SetFloat("_threshold", this.threshold);
+                }
+            }
+        }
+
+        public void show()
+        {
+            this.is_hiding = false;
+
+            this.threshold = this.max_threshold;
+
+            for (int i = 0; i < this.materials.Length; i++)
+            {
+                this.materials[i].SetFloat("_threshold", this.threshold);
+            }
+            this.is_showing = true;
+        }
+
+        public void hide()
+        {
+            this.is_showing = false;
+
+            this.threshold = this.min_threshold;
+
+            for (int i = 0; i < this.materials.Length; i++)
+            {
+                this.materials[i].SetFloat("_threshold", this.threshold);
+            }
+
+            this.is_hiding = true;
+        }
+    }
+}
